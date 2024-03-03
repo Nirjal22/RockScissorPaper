627d22fa15947ab08334956cca60140320f106e2
nirjal <bistanirjal978@gmail.com>
2024-03-03 18:01:02 +0545

    Fix: case1, need to fix case2

--- Rock.java
+++ Rock.java
@@ -10,7 +10,7 @@ public class Rock {
 		String[] choices = { "R", "S", "P" };
 		String randomChoices = choices[randomNumber];
 		// variables
-		String personPlay, secondPlayer, computerPlay;
+		String personPlay, secondPlayer;
 		int computerInt;
 
 		Console console = System.console();
@@ -20,30 +20,64 @@ public class Rock {
 		System.out.println("2. Multiplayer");
 		System.out.println("3. Exit");
 		// taking input
-		computerInt = scan.nextInt();
+		while (true) { // infinite loop until it satisfy the statement.
+			System.out.println("Enter an number (1 to 3): ");
+			String computer = scan.next();
+			// exception handling
+			try {
+				int i = Integer.parseInt(computer);// changing String to Int
+				if (i > 0 && i < 4) {
+					computerInt = i;
+					break;
+				} else {
+					System.out.println("Invalid Input!\n Enter valid Input: ");
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid Input!!");
+			}
+		}
 		switch (computerInt) // switch statement
 		{
 			case 1: // for single player
 				System.out.println("Running single player\n");
-				char[] entering = console.readPassword("Choose ('R' for Rock, 'S' for Scissor, or 'P' for Paper): ");
-				personPlay = new String(entering).toUpperCase();
-				computerPlay = randomChoices; // assigning the values to computerPlay
+				String personPlay1 = ""; // initalizing the variables outside of the while loop.
+				boolean inValidInput = false;
+				while (!inValidInput) {
+					char[] entering = console
+							.readPassword("Choose ('R' for Rock, 'S' for Scissor, or 'P' for Paper): ");
+					personPlay1 = new String(entering).toUpperCase();
+					try {
+						for (int j = 0; j < choices.length; j++) {// for loop to check if the array have same values or
+																	// not.
+							if (personPlay1.equals(choices[j])) {
+								inValidInput = true;
+								// if the values are matched then it throws true and break the statement.
+								break;
+							}
+						}
+					} catch (Exception e) {
+						System.out.println("Enter the correct values: ");
+						continue;
+					}
+				}
+
+				String computerPlay = randomChoices; // assigning the values to computerPlay
 				// if-else condition
-				if (computerPlay.equals(personPlay)) {
+				if (computerPlay.equals(personPlay1)) {
 					System.out.println("It's a tie!");
-				} else if (personPlay.equals("R")) {
+				} else if (personPlay1.equals("R")) {
 					if (computerPlay.equals("S")) {
 						System.out.println("Rock crushes scissors. You win!");
 					} else if (computerPlay.equals("P")) {
 						System.out.println("Paper covers Rock. Computer wins!");
 					}
-				} else if (personPlay.equals("P")) {
+				} else if (personPlay1.equals("P")) {
 					if (computerPlay.equals("S")) {
 						System.out.println("Scissors cuts Paper. Computer wins!");
 					} else if (computerPlay.equals("R")) {
 						System.out.println("Paper covers Rock. You win!");
 					}
-				} else if (personPlay.equals("S")) {
+				} else if (personPlay1.equals("S")) {
 					if (computerPlay.equals("R")) {
 						System.out.println("Rock crushes scissors. Computer wins!");
 					} else if (computerPlay.equals("P")) {
@@ -88,6 +122,7 @@ public class Rock {
 				} else {// throws an invalid input if user tries to enter other values.
 					System.out.println("Invalid input. Please choose R, P, or S.");
 				}
+				break;
 			case 3:
 				break;
 			default:
